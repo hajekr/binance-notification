b402eb401feaa3823196d93016780e21930e4805
Roman Hajek <roman.hajek@bsc-ideas.com>
2021-04-27 21:25:24 +0200

    Slack SDK test

--- notify-binance.py
+++ notify-binance.py
@@ -1,1 +1,21 @@
-print("This line will be printed foooo bar.")
+import os
+# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# WebClient instantiates a client that can call API methods
+# When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+# ID of the channel you want to send the message to
+channel_id = "C12345"
+
+try:
+    # Call the chat.postMessage method using the WebClient
+    result = client.chat_postMessage(
+        channel=channel_id,
+        text="Hello world"
+    )
+    logger.info(result)
+
+except SlackApiError as e:
+    logger.error("Error posting message: {e}")
